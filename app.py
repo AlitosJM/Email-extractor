352539b06d0612f687dbe95b32b348f5a089febf
AliTosJM <alitosjm@gmail.com>
2021-01-22 17:45:16 -0600

    features home should be SingleExtractor

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ def main():
         st.subheader("Search & Extract")
     elif choice == "Sigle Extractor":
         st.subheader("Extract A Single Term")
+        text = st.text_area("Paste Text Here")
+        list_of_items = ["Emails", "URLS", "Phonenumbers"]
+        task_option = st.sidebar.selectbox("Task", list_of_items)
+
+        if st.button("Extract"):
+            if task_option == list_of_items[0]:
+                results = nfx.extract_emails(text)
+            elif task_option == list_of_items[1]:
+                results = nfx.extract_urls(text)
+            elif task_option == list_of_items[2]:
+                results = nfx.extract_phone_numbers(text)
+
+            st.write(results)
+
+            with st.beta_expander("Results As Dataframe"):
+                result_df = pd.DataFrame({"Results": results})
+                st.dataframe(result_df)
+
     elif choice == "Bulk Extractor":
         st.subheader("Bulk Extractor")
     else:
