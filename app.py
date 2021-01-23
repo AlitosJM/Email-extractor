1b7183cf4d3faf09a1ab37c595ab0df1683c2296
AliTosJM <alitosjm@gmail.com>
2021-01-22 19:14:24 -0600

    Fxn functions

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ import streamlit as st
 import streamlit.components.v1 as static
 import pandas as pd
 import neattext.functions as nfx
+import base64
+import time
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+
+# Fxn to Download
+def make_downloadable(data, task_type):
+    csvfile = data.to_csv(index=False)
+    b64 = base64.b64encode(csvfile.encode()).decode()  # B64 encoding
+    st.markdown("### ** Download Results File ** ")
+    new_filename = "extracted_{}_result_{}.csv".format(task_type, timestr)
+    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+
+# Fxn to Download
+def make_downloadable_df(data):
+    csvfile = data.to_csv(index=False)
+    b64 = base64.b64encode(csvfile.encode()).decode()  # B64 encoding
+    st.markdown("### ** Download CSV File ** ")
+    new_filename = "extracted_data_result_{}.csv".format(timestr)
+    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 
 def main():
